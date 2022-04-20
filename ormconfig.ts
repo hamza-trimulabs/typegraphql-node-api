74b4c73cbea82c879e434c9fb85956f2e9ae4fcf
hamzatrimu <hamza.ashraf@trimulabs.com>
2022-04-20 14:22:58 +0500

    ormconfig set for production/staging

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,16 +1,31 @@
-export default {
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "test",
-  synchronize: true,
-  entities: ["src/db/entity/*.ts"],
-  migrations: ["src/db/migration/*.ts"],
-  migrationsTableName: "migrations",
-  cli: {
-    entitiesDir: ["src/db/entity"],
-    migrationsDir: "src/db/migration",
-  },
-};
+export default process.env.NODE_ENV === "production" ||
+process.env.NODE_ENV === "staging"
+  ? {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      entities: ["src/db/entity/*.ts"],
+      migrations: ["src/db/migration/*.ts"],
+      migrationsTableName: "migrations",
+      cli: {
+        entitiesDir: ["src/db/entity"],
+        migrationsDir: "src/db/migration",
+      },
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      entities: ["src/db/entity/*.ts"],
+      migrations: ["src/db/migration/*.ts"],
+      migrationsTableName: "migrations",
+      cli: {
+        entitiesDir: ["src/db/entity"],
+        migrationsDir: "src/db/migration",
+      },
+    };
+
+// postgres://mtstkiufhxcgoi:436a86feaa4257928bdf26b57e3162eb2147e2b235abf29d875ef98628cadf89@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d68ok4j9ce106d
